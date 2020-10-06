99a0c33ffa4bcdbb290d11725f6f14673d19425d
David Anderson <djathree92@gmail.com>
2020-10-06 16:08:41 -0400

    css

--- main_app/views.py
+++ main_app/views.py
@@ -15,3 +15,7 @@ def about(request):
 def monsters_index(request):
     monsters = Monster.objects.all()
     return render(request, 'monsters/index.html', {'monsters': monsters})
+
+def monsters_detail(request, monster_id):
+    monster = Monster.objects.get(id=monster_id)
+    return render(request, 'monsters/detail.html', {'monster': monster})

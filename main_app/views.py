6efb44ceedcdec372f752bcdfa61a5048cc3575b
David Anderson <djathree92@gmail.com>
2020-10-06 15:00:53 -0400

    add monsters via admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,17 @@
 from django.shortcuts import render
+from .models import Monster
 
 # Create your views here.
 from django.http import HttpResponse
 
-class Monster:
-    def __init__(self, name, size, element, ferocity):
-        self.name = name
-        self.size = size
-        self.element = element
-        self.ferocity = ferocity
-
-monsters = [
-    Monster('Fatalis', 'large', 'fire', 'HIGH PAST THE SKY'),
-    Monster('Alatreon', 'large', 'fire, water, thunder, dragon', 'high'),
-    Monster('Vespoid', 'small', 'none', 'medium')
-]
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def monsters_index(request):
+    monsters = Monster.objects.all()
     return render(request, 'monsters/index.html', {'monsters': monsters})
